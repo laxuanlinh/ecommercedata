f8d8ff35f9cde378d239397cc002165d0ffab340
Lã Xuân Linh <laxuanlong@gmail.com>
2020-08-04 17:22:22 +0800

    fix some dependencies

--- src/main/kotlin/com/linh/EcommerceData/config/RabbitMQConfig.kt
+++ src/main/kotlin/com/linh/EcommerceData/config/RabbitMQConfig.kt
@@ -1,6 +1,5 @@
 package com.linh.EcommerceData.config
 
-import com.rabbitmq.client.Channel
 import com.rabbitmq.client.ConnectionFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
--- src/test/kotlin/com/linh/EcommerceData/config/RabbitMQConfigTest.kt
+++ src/test/kotlin/com/linh/EcommerceData/config/RabbitMQConfigTest.kt
@@ -1,13 +1,7 @@
 package com.linh.EcommerceData.config
 
 import org.junit.Assert
-import org.junit.Before
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
-import org.junit.runner.RunWith
-import org.mockito.InjectMocks
-import org.mockito.Mock
-import org.springframework.beans.factory.annotation.Value
 
 
 class RabbitMQConfigTest{
